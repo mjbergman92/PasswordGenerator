41ea275f17717e459820f24e3c72cd29365d660b
Malachi Bergman <malachijb28@gmail.com>
2019-01-15 11:44:35 -0500

    complete the generation processes for each option of password generaton

--- PasswordGenerator.cpp
+++ PasswordGenerator.cpp
@@ -7,10 +7,10 @@
 using namespace std;
 int main() {
 
-	int passwordType;
-	int passwordLength;
+	int passwordType;		//the choice of password, from 1 to 5, to be input from the user
+	int passwordLength;		//the choice of length of the password, from 6 to 14, to be input from the user
 	string options[] = { "Lowercase Letters\t\t\t\t\t", "Lowercase and Numbers\t\t\t\t", "Lowercase, Uppercase, & Numbers\t\t\t", "Lowercase, Uppercase, Numbers, & Punctuaction\t", "Quit\t\t\t\t\t\t" };
-
+	bool running = true;	//will be used to stop the generation process
 
 	srand(time(NULL));
 
@@ -37,7 +37,7 @@ int main() {
 
 	cout << endl << "Click Enter" << endl;
 	
-	while (true) {
+	while (running) {
 
 		do {
 			
@@ -53,26 +53,24 @@ int main() {
 
 		} while (passwordType < 1 || passwordType > 5);
 
-		do {
-
-			bool flag;
+		if (passwordType != 5) {
 
 			do {
 
-				cin.clear();
-				cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-				cout << "Password Length (6 to 14 characters): ";
-				cin >> passwordLength;
+				do {
 
-				flag = cin.fail();
-				cout << flag;
+					cin.clear();
+					cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+					cout << "Password Length (6 to 14 characters): ";
+					cin >> passwordLength;
 
+				} while (cin.fail());
 
-			} while (flag);
+			} while (passwordLength < 6 || passwordLength > 14);
 
-		} while (passwordLength < 6 || passwordLength > 14);
+		}
 
-		cout << endl << "Hello People";
+		cout << endl;
 
 		switch (passwordType) {
 
@@ -88,19 +86,125 @@ int main() {
 
 			}
 
+			cout << endl << endl;
+
 			break;
 		case 2:
+
+			cout << "Generated Password: ";
+
+			for (int i = 0; i < passwordLength; i = i++) {
+
+				int chosenNumber = rand() % 36;
+				if (chosenNumber < 10) {
+
+					chosenNumber += 48;
+
+				}
+				else {
+
+					chosenNumber += -10 + 97;
+
+				}
+				char chosenCharacter = static_cast<char>(chosenNumber);
+				cout << chosenCharacter;
+
+			}
+
+			cout << endl << endl;
 
 			break;
 		case 3:
 
+			cout << "Generated Password: ";
+
+			for (int i = 0; i < passwordLength; i = i++) {
+
+				int chosenNumber = rand() % 62;
+				if (chosenNumber < 10) {
+
+					chosenNumber += 48;
+
+				}
+				else if(chosenNumber < 36){
+
+					chosenNumber += -10 + 65;
+
+				}
+				else {
+
+					chosenNumber += -36 + 97;
+
+				}
+				char chosenCharacter = static_cast<char>(chosenNumber);
+				cout << chosenCharacter;
+
+			}
+
+			cout << endl << endl;
+
 			break;
 		case 4:
+
+			cout << "Generated Password: ";
+
+			for (int i = 0; i < passwordLength; i = i++) {
+
+				int chosenNumber = rand() % 81;
+				if (chosenNumber < 7) {
+
+					chosenNumber += 33;
+
+				}
+				else if (chosenNumber < 8) {
+
+					chosenNumber += -7 + 42;
+
+				}
+				else if(chosenNumber < 11){
+
+					chosenNumber += -8 + 44;
+
+				}
+				else if (chosenNumber < 23) {
+
+					chosenNumber += -11 + 48;
+
+				}
+				else if (chosenNumber < 51) {
+
+					chosenNumber += -23 + 63;
+
+				}
+				else if (chosenNumber < 80) {
+
+					chosenNumber += -51 + 94;
+
+				}
+				else {
+
+					chosenNumber += -80 + 126;
+
+				}
+				char chosenCharacter = static_cast<char>(chosenNumber);
+				cout << chosenCharacter;
+
+			}
+
+			cout << endl << endl;
 
 			break;
 		case 5:
 
+			cout << "Quitting Now..." << endl << endl;
+
 			break;
+
+		}
+
+		if (passwordType == 5) {
+
+			running = false;
 
 		}
 
